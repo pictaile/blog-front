8c5ed75c35702107221c63abd44691c377ba10c9
Охотник Костянтин Олександрович <konstantik.ohotnik@a-bank.com.ua>
2019-04-03 14:31:33 +0300

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {AdminComponent} from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
