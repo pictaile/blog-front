d762e18a58a0044077010a059a3ecf55cbddb63c
Охотник Костянтин Олександрович <konstantik.ohotnik@a-bank.com.ua>
2019-04-11 20:20:30 +0300

    add sid

--- src/app/general/xhr/articles/articles.service.ts
+++ src/app/general/xhr/articles/articles.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import {BaseSenderService} from '../base/base-sender.service';
 import {MAIN_URL, URL_ENUM} from '../../enums/urls.enum';
 import {HttpClient} from '@angular/common/http';
+import {OAuthService} from 'angular-oauth2-oidc';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService extends BaseSenderService {
 
-  constructor(   private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private oauthService: OAuthService
+    ) {
     super();
   }
 
@@ -22,7 +26,8 @@ export class ArticlesService extends BaseSenderService {
 
   public save(data) {
     const options = {
-      ...data
+      ...data,
+      sid: this.oauthService.getAccessToken()
     };
 
     const request$ = this.http.post(`${MAIN_URL}/${URL_ENUM.ARTICLES}/${data.id}`, options);
